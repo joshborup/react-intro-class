b54563866fd5f6f1af968094fc79adddbab104ba
joshua borup <joshuaborup@joshuas-MacBook-Pro.local>
2018-01-26 16:24:19 -0700

    finished Calc with Black Diamond

---
+++ app/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Calculator from './components/Calculator.js';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
+}
+
+export default App;
